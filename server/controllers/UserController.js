9521a32d61bbd61b4e67a32463a120d44afd833a
jackmazac <mazac@chapman.edu>
2023-11-30 19:30:00 -0800

    aider: Create UserController.js with methods for handling user operations.
    # Aider chat conversation:
    
    USER: Create server/controllers/UserController.js (Handles user operations)
    
    ASSISTANT: Sure, I can help you create a new file named `server/controllers/UserController.js`. Here's a basic example of how you might define such a controller:
    
    server/controllers/UserController.js
    ```javascript
    <<<<<<< SEARCH
    =======
    const UserModel = require('../models/UserModel');
    
    const UserController = {
        getAllUsers: function(req, res) {
            UserModel.get(function(err, users) {
                if (err) {
                    res.status(500).send(err);
                } else {
                    res.json(users);
                }
            });
        },
    
        getUserById: function(req, res) {
            UserModel.getById(req.params.id, function(err, user) {
                if (err) {
                    res.status(500).send(err);
                } else {
                    res.json(user);
                }
            });
        },
    
        addUser: function(req, res) {
            UserModel.add(req.body, function(err, user) {
                if (err) {
                    res.status(500).send(err);
                } else {
                    res.json(user);
                }
            });
        },
    
        deleteUser: function(req, res) {
            UserModel.delete(req.params.id, function(err, user) {
                if (err) {
                    res.status(500).send(err);
                } else {
                    res.json(user);
                }
            });
        },
    
        updateUser: function(req, res) {
            UserModel.update(req.params.id, req.body, function(err, user) {
                if (err) {
                    res.status(500).send(err);
                } else {
                    res.json(user);
                }
            });
        }
    };
    
    module.exports = UserController;
    >>>>>>> REPLACE
    ```
    
    This code defines a `UserController` with methods for getting all users, getting a user by ID, adding a user, deleting a user, and updating a user. Each method uses the corresponding method from `UserModel` to interact with the database, and sends the result as a JSON response.
    
    Please note that this is a basic example and you may need to adjust it to fit your specific needs. For example, you might want to add error handling, validation, or authentication.

---
+++ server/controllers/UserController.js
@@ -0,0 +1,55 @@
+const UserModel = require('../models/UserModel');
+
+const UserController = {
+    getAllUsers: function(req, res) {
+        UserModel.get(function(err, users) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(users);
+            }
+        });
+    },
+
+    getUserById: function(req, res) {
+        UserModel.getById(req.params.id, function(err, user) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(user);
+            }
+        });
+    },
+
+    addUser: function(req, res) {
+        UserModel.add(req.body, function(err, user) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(user);
+            }
+        });
+    },
+
+    deleteUser: function(req, res) {
+        UserModel.delete(req.params.id, function(err, user) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(user);
+            }
+        });
+    },
+
+    updateUser: function(req, res) {
+        UserModel.update(req.params.id, req.body, function(err, user) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(user);
+            }
+        });
+    }
+};
+
+module.exports = UserController;
