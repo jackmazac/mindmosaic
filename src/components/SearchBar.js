79bcad3d8cd06d5a539e06df089fada265d6ba66
jackmazac <mazac@chapman.edu>
2023-11-30 19:48:43 -0800

    aider: Added functionality to the search bar component.
    # Aider chat conversation:
    
    USER: Here is a list of repo files for reference "  .gitignore
      README.md
      database/init.sql
      database/seed.sql
      package-lock.json
      package.json
      public/favicon.ico
      public/index.html
      public/logo192.png
      public/logo512.png
      public/manifest.json
      public/robots.txt
      server/config/dbConfig.js
      server/controllers/ContentController.js
      server/controllers/InteractionController.js
      server/controllers/SocialController.js
      server/controllers/UserController.js
      server/models/ContentModel.js
      server/models/InteractionModel.js
      server/models/PreferenceModel.js
      server/models/SocialModel.js
      server/models/UserModel.js
      server/routes/contentRoutes.js
      server/routes/interactionRoutes.js
      server/routes/socialRoutes.js
      server/routes/userRoutes.js
      server/server.js
      server/utils/responseHandler.js
      server/utils/validator.js
      src/App.css
      src/App.js
      src/App.test.js
      src/api.js
      src/components/ContentCard.js
      src/components/ContentList.js
      src/components/Footer.js
      src/components/SearchBar.js
      src/components/SocialInteraction.js
      src/components/UserProfile.js
      src/hooks/useContentData.js
      src/hooks/useUserData.js
      src/index.css
      src/index.js
      src/logo.svg
      src/pages/ContentPage.js
      src/pages/HomePage.js
      src/pages/SettingsPage.js
      src/pages/UserDashboard.js
      src/reportWebVitals.js
      src/setupTests.js"
    
    ASSISTANT: Ok.
    
    USER: I added these *read-write* files: /Users/jackmazac/Desktop/mindmosaic/src/components/SearchBar.js
    
    ASSISTANT: Ok.
    
    USER: Make search bar functiona
    
    ASSISTANT: To make the search bar functional, we need to add a state to the SearchBar component to keep track of the search term and a function to handle the changes in the input field. We also need to add an onSubmit handler to the form to handle the form submission.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src/components/SearchBar.js
    ```javascript
    <<<<<<< SEARCH
    import React from 'react';
    
    const SearchBar = () => {
        return (
            <div>
                <input type="text" placeholder="Search..." />
            </div>
        );
    }
    
    export default SearchBar;
    =======
    import React, { useState } from 'react';
    
    const SearchBar = () => {
        const [searchTerm, setSearchTerm] = useState('');
    
        const handleChange = (event) => {
            setSearchTerm(event.target.value);
        }
    
        const handleSubmit = (event) => {
            event.preventDefault();
            // Here you can handle the search term, for example, send it to the API
            console.log(searchTerm);
        }
    
        return (
            <form onSubmit={handleSubmit}>
                <input type="text" placeholder="Search..." value={searchTerm} onChange={handleChange} />
            </form>
        );
    }
    
    export default SearchBar;
    >>>>>>> REPLACE
    ```
    
    This code will make the search bar functional by keeping track of the user's input and handling the form submission. The search term is logged to the console when the form is submitted. You can replace the `console.log(searchTerm);` line with your own code to handle the search term, for example, send it to the API.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Here you can handle the search term, for example, send it to the API
+        console.log(searchTerm);
+    }
+
     return (
-        <div>
-            <input type="text" placeholder="Search..." />
-        </div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Search..." value={searchTerm} onChange={handleChange} />
+        </form>
     );
 }
 
