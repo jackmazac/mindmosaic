00c75979b1d3301887c411f5fda8ae3b54041bd1
jackmazac <mazac@chapman.edu>
2023-11-30 19:24:27 -0800

    aider: Created UserModel.js for MySQL.
    # Aider chat conversation:
    
    USER: Now create server/models/UserModel.js for mysql
    
    ASSISTANT: To create `server/models/UserModel.js` for MySQL, we need to:
    
    1. Import the mysql package.
    2. Create a User model with the necessary fields.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    server/models/UserModel.js
    ```javascript
    <<<<<<< SEARCH
    =======
    const db = require('../config/dbConfig');
    
    const User = {
        get: function(callback) {
            return db.query('SELECT * from users', callback);
        },
    
        getById: function(id, callback) {
            return db.query('SELECT * from users where id=?', [id], callback);
        },
    
        add: function(User, callback) {
            return db.query(
                'INSERT into users (username,password,email) values(?,?,?)',
                [User.username, User.password, User.email],
                callback
            );
        },
    
        delete: function(id, callback) {
            return db.query('DELETE from users where id=?', [id], callback);
        },
    
        update: function(id, User, callback) {
            return db.query(
                'UPDATE users set username=?, password=?, email=? where id=?',
                [User.username, User.password, User.email, id],
                callback
            );
        }
    };
    
    module.exports = User;
    >>>>>>> REPLACE
    ```
    
    Please note that this is a basic structure for a User model using MySQL. You might need to adjust the code as per your requirements. Also, make sure that the `dbConfig.js` file is correctly set up and the MySQL database is running.

---
+++ server/models/UserModel.js
@@ -0,0 +1,33 @@
+const db = require('../config/dbConfig');
+
+const User = {
+    get: function(callback) {
+        return db.query('SELECT * from users', callback);
+    },
+
+    getById: function(id, callback) {
+        return db.query('SELECT * from users where id=?', [id], callback);
+    },
+
+    add: function(User, callback) {
+        return db.query(
+            'INSERT into users (username,password,email) values(?,?,?)',
+            [User.username, User.password, User.email],
+            callback
+        );
+    },
+
+    delete: function(id, callback) {
+        return db.query('DELETE from users where id=?', [id], callback);
+    },
+
+    update: function(id, User, callback) {
+        return db.query(
+            'UPDATE users set username=?, password=?, email=? where id=?',
+            [User.username, User.password, User.email, id],
+            callback
+        );
+    }
+};
+
+module.exports = User;
