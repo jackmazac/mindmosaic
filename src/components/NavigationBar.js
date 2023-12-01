f7f6c0da692e11e8c4b81ebc62e65be1451c2deb
jackmazac <mazac@chapman.edu>
2023-11-30 20:08:35 -0800

    aider: To make the nav bar buttons horizontal and remove the bullet points, we need to modify `src/components/NavigationBar.js` and `src/App.css`.
    # Aider chat conversation:
    
    USER: The nav bar buttons are vertial when they should be horizontal. They also have bullet points when they should not
    
    ASSISTANT: To make the nav bar buttons horizontal and remove the bullet points, we need to modify `src/components/NavigationBar.js` and `src/App.css`:
    
    1. Add a class to the `ul` element in `NavigationBar.js`.
    2. Add CSS rules to `App.css` to style the `ul` and `li` elements.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src/components/NavigationBar.js
    ```javascript
    <<<<<<< SEARCH
            <nav>
                <ul>
    =======
            <nav>
                <ul className="navbar">
    >>>>>>> REPLACE
    ```
    
    src/App.css
    ```css
    <<<<<<< SEARCH
    =======
    .navbar {
        list-style-type: none;
        margin: 0;
        padding: 0;
        overflow: hidden;
        background-color: #333;
    }
    
    .navbar li {
        float: left;
    }
    
    .navbar li a {
        display: block;
        color: white;
        text-align: center;
        padding: 14px 16px;
        text-decoration: none;
    }
    
    .navbar li a:hover {
        background-color: #111;
    }
    >>>>>>> REPLACE
    ```

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,7 +5,7 @@ import AboutPage from '../pages/AboutPage';
 const NavigationBar = () => {
     return (
         <nav>
-            <ul>
+            <ul className="navbar">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
