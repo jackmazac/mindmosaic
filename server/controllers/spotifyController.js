96b2079c0bffa04d3cf126161cd2e2f5cf9def02
jackmazac <mazac@chapman.edu>
2023-12-05 12:42:27 -0800

    update

---
+++ server/controllers/spotifyController.js
@@ -0,0 +1,14 @@
+const db = require('../db'); // Assuming you have a db.js file for database connection
+
+const getSampleData = (req, res) => {
+    db.query('SELECT * FROM Songs', (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+        res.json(results);
+    });
+};
+
+module.exports = {
+    getSampleData
+};
