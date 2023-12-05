ee890f18f57407dbee749f5cba95a0f6805f9843
jackmazac <mazac@chapman.edu>
2023-12-05 12:32:55 -0800

    aider: Added sample data to the database about Spotify.
    # Aider chat conversation:
    
    USER: Add sample data to our database about spotify
    
    ASSISTANT: To add sample data to your database, we need to modify `database/seed.sql`.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    database/seed.sql
    ```sql
    <<<<<<< SEARCH
    -- Insert data into Interactions
    INSERT INTO Interactions (UserID, ContentID, InteractionType)
    VALUES (1, 2, 'Like'),
           (2, 3, 'Dislike'),
           (3, 1, 'Like');
    =======
    -- Insert data into Artists
    INSERT INTO Artists (Name, Biography)
    VALUES ('Artist 1', 'Biography for Artist 1'),
           ('Artist 2', 'Biography for Artist 2');
    
    -- Insert data into Albums
    INSERT INTO Albums (ArtistID, Title, ReleaseDate)
    VALUES (1, 'Album 1', '2022-01-01'),
           (2, 'Album 2', '2022-02-02');
    
    -- Insert data into Songs
    INSERT INTO Songs (AlbumID, Title, Duration)
    VALUES (1, 'Song 1', '00:03:30'),
           (2, 'Song 2', '00:04:00');
    
    -- Insert data into Playlists
    INSERT INTO Playlists (UserID, Name, Description)
    VALUES (1, 'Playlist 1', 'Description for Playlist 1'),
           (2, 'Playlist 2', 'Description for Playlist 2');
    
    -- Insert data into Favorites
    INSERT INTO Favorites (UserID, SongID)
    VALUES (1, 1),
           (2, 2);
    
    -- Insert data into UserInteractions
    INSERT INTO UserInteractions (UserID_1, UserID_2, InteractionType)
    VALUES (1, 2, 'Follow'),
           (2, 3, 'Follow'),
           (3, 1, 'Follow');
    >>>>>>> REPLACE
    ```
    
    This will add sample data for Artists, Albums, Songs, Playlists, Favorites, and UserInteractions.

--- database/seed.sql
+++ database/seed.sql
@@ -13,8 +13,33 @@ VALUES (1, 'Content from user1'),
        (2, 'Content from user2'),
        (3, 'Content from user3');
 
--- Insert data into Interactions
-INSERT INTO Interactions (UserID, ContentID, InteractionType)
-VALUES (1, 2, 'Like'),
-       (2, 3, 'Dislike'),
-       (3, 1, 'Like');
+-- Insert data into Artists
+INSERT INTO Artists (Name, Biography)
+VALUES ('Artist 1', 'Biography for Artist 1'),
+       ('Artist 2', 'Biography for Artist 2');
+
+-- Insert data into Albums
+INSERT INTO Albums (ArtistID, Title, ReleaseDate)
+VALUES (1, 'Album 1', '2022-01-01'),
+       (2, 'Album 2', '2022-02-02');
+
+-- Insert data into Songs
+INSERT INTO Songs (AlbumID, Title, Duration)
+VALUES (1, 'Song 1', '00:03:30'),
+       (2, 'Song 2', '00:04:00');
+
+-- Insert data into Playlists
+INSERT INTO Playlists (UserID, Name, Description)
+VALUES (1, 'Playlist 1', 'Description for Playlist 1'),
+       (2, 'Playlist 2', 'Description for Playlist 2');
+
+-- Insert data into Favorites
+INSERT INTO Favorites (UserID, SongID)
+VALUES (1, 1),
+       (2, 2);
+
+-- Insert data into UserInteractions
+INSERT INTO UserInteractions (UserID_1, UserID_2, InteractionType)
+VALUES (1, 2, 'Follow'),
+       (2, 3, 'Follow'),
+       (3, 1, 'Follow');
