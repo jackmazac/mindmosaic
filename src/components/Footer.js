2fad5711e3e97b7d7eff145dfb2a68a462f7eb92
jackmazac <mazac@chapman.edu>
2023-12-05 11:53:53 -0800

    aider: Make the footer always at the bottom of the page.
    # Aider chat conversation:
    
    USER: Make the footer always at the bottom of the page
    
    ASSISTANT: To make the footer always at the bottom of the page, we need to add some CSS styles to the Footer component.
    
    We will modify `src/components/Footer.js`:
    
    1. Add a className to the footer tag.
    2. Add a style block with CSS to position the footer at the bottom of the page.
    
    Here is the *SEARCH/REPLACE* block:
    
    src/components/Footer.js
    ```javascript
    <<<<<<< SEARCH
            <footer>
    =======
            <footer className="footer">
    >>>>>>> REPLACE
    ```
    
    src/components/Footer.js
    ```javascript
    <<<<<<< SEARCH
    export default Footer;
    =======
    <style jsx>{`
        .footer {
            position: fixed;
            left: 0;
            bottom: 0;
            width: 100%;
            background-color: #f8f9fa;
            color: black;
            text-align: center;
        }
    `}</style>
    export default Footer;
    >>>>>>> REPLACE
    ```
    
    Please note that this solution assumes that you are using a CSS-in-JS solution like styled-jsx. If you are using a different styling solution (like CSS modules, styled-components, or a global CSS file), the exact code may vary.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,21 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer>
+        <footer className="footer">
             <p>Copyright &copy; 2022</p>
         </footer>
     );
 }
 
+<style jsx>{`
+    .footer {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        background-color: #f8f9fa;
+        color: black;
+        text-align: center;
+    }
+`}</style>
 export default Footer;
