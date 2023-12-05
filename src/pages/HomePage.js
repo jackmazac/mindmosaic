248c11d4a02d73b8ca4ca8f18a96e9a10e3a0dbc
jackmazac <mazac@chapman.edu>
2023-12-05 12:08:03 -0800

    Changed the name from MindMosaic to Tunify and updated the content accordingly.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,14 +6,13 @@ import Footer from '../components/Footer';
 
 const HomePage = () => {
     const contents = [
-        { title: 'MindMosaic', content: 'MindMosaic is a platform that allows users to explore and interact with a variety of content in a unique and engaging way.' },
+        { title: 'Tunify', content: 'Tunify is a platform that allows users to explore and interact with a variety of content in a unique and engaging way.' },
         { title: 'Our Goal', content: 'Our goal is to provide a space where users can discover new ideas, connect with others, and express themselves creatively.' },
         { title: 'Join Us', content: 'Join us in creating a community of creative minds and explorers.' },
     ];
 
     return (
         <div>
-            <p>MindMosaic is a platform that allows users to explore and interact with a variety of content in a unique and engaging way. Our goal is to provide a space where users can discover new ideas, connect with others, and express themselves creatively.</p>
             <ContentList contents={contents} />
             <Footer />
         </div>
