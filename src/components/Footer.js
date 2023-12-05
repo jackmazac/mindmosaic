083a683abd83537bea3ec8b8293498ef62ff5950
jackmazac <mazac@chapman.edu>
2023-12-05 11:54:56 -0800

    aider: Created a CSS file for the footer styles and removed the JSX styles from the Footer component.
    # Aider chat conversation:
    
    USER: I have been using JS + CSS instead of JSX, make a css file instead of JSX
    
    ASSISTANT: To make this change, we need to create a new CSS file `src/components/Footer.css` and modify `src/components/Footer.js`:
    
    1. Create a new CSS file `src/components/Footer.css` with the styles for the footer.
    2. Remove the JSX styles from `src/components/Footer.js`.
    3. Import the new CSS file in `src/components/Footer.js`.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    src/components/Footer.css
    ```css
    <<<<<<< SEARCH
    =======
    .footer {
        position: fixed;
        left: 0;
        bottom: 0;
        width: 100%;
        background-color: #f8f9fa;
        color: black;
        text-align: center;
    }
    >>>>>>> REPLACE
    ```
    
    src/components/Footer.js
    ```javascript
    <<<<<<< SEARCH
    <style jsx>{`
        .footer {
            position: fixed;
            left: 0;
            bottom: 0;
            width: 100%;
            background-color: #f8f9fa;
            color: black;
            text-align: center;
        }
    `}</style>
    =======
    import './Footer.css';
    >>>>>>> REPLACE
    ```
    
    Please note that this solution assumes that you are using a CSS file for styling. If you are using a different styling solution (like CSS modules, styled-components, or a global CSS file), the exact code may vary.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,15 +8,5 @@ const Footer = () => {
     );
 }
 
-<style jsx>{`
-    .footer {
-        position: fixed;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-        background-color: #f8f9fa;
-        color: black;
-        text-align: center;
-    }
-`}</style>
+import './Footer.css';
 export default Footer;
