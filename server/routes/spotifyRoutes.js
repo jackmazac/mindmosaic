fe591877946fea62cc2773ccd4ec01a31c47349a
jackmazac <mazac@chapman.edu>
2023-12-14 15:32:04 -0800

    aider: Fixed route path for fetching songs in `server/routes/spotifyRoutes.js`.
    # Aider chat conversation:
    
    USER: Now error is "Spotify.js:25
    
           GET http://localhost:3000/api/spotify/songs 404 (Not Found)"
    
    ASSISTANT: The error message indicates that the request to `http://localhost:3000/api/spotify/songs` is returning a 404 Not Found status, which means the route is not correctly set up on the server. To fix this, we need to adjust the route definition in `server/routes/spotifyRoutes.js` to match the expected endpoint.
    
    Here are the steps to fix the issue:
    
    1. Correct the route path for fetching songs in `server/routes/spotifyRoutes.js`.
    
    Here is the *SEARCH/REPLACE* block for the necessary change:
    
    server/routes/spotifyRoutes.js
    ```javascript
    <<<<<<< SEARCH
    router.get('/api/spotify/songs', spotifyController.getSongs);
    =======
    router.get('/songs', spotifyController.getSongs);
    >>>>>>> REPLACE
    ```
    
    This change will make the route match the expected endpoint that the front-end `Spotify.js` component is trying to reach. After making this change, the server should correctly handle requests to `http://localhost:3000/api/spotify/songs`.

--- server/routes/spotifyRoutes.js
+++ server/routes/spotifyRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const spotifyController = require('../controllers/spotifyController');
 
 // Route to fetch songs with optional filtering
-router.get('/api/spotify/songs', spotifyController.getSongs);
+router.get('/songs', spotifyController.getSongs);
 
 // Validation rules for adding a new song
 const songValidationRules = [
