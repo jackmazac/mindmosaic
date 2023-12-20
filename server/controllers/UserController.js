eeb32e83964daa5a776c8fcb77e0173efe84c5b9
jackmazac <mazac@chapman.edu>
2023-12-19 21:06:48 -0800

    final push

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,13 +1,13 @@
 const UserModel = require('../models/UserModel');
 const crypto = require('crypto');
 const jwt = require('jsonwebtoken');
-const secretKey = 'your-secret-key'; // You should replace this with your actual secret key
+const secretKey = 'your-secret-key'; // Replace with your actual secret key
 
 const UserController = {
     getAllUsers: function(req, res) {
         UserModel.get(function(err, users) {
             if (err) {
-                res.status(500).json({ error: 'Error logging in', details: err });
+                res.status(500).json({ error: 'Error fetching users', details: err });
             } else {
                 res.json(users);
             }
@@ -22,10 +22,7 @@ const UserController = {
                 return res.status(500).json({ error: 'Error hashing password' });
             }
             const hash = derivedKey.toString('hex');
-            if (err) {
-                return res.status(500).json({ error: 'Error hashing password' });
-            }
-            UserModel.register({ username, email, password: hash }, function(err, user) {
+            UserModel.register({ username, email, password: hash, salt }, function(err, user) {
                 if (err) {
                     res.status(500).json({ error: 'Error registering new user', details: err });
                 } else {
@@ -41,39 +38,15 @@ const UserController = {
         UserModel.getByUsername(username, function(err, user) {
             if (err) {
                 res.status(500).json({ error: 'Error logging in', details: err });
-            } else {
-                if (user) {
-                    crypto.pbkdf2(password, user.salt, 1000, 64, 'sha512', (err, derivedKey) => {
-                        if (err) {
-                            return res.status(500).json({ error: 'Error verifying password' });
-                        }
-                        const hash = derivedKey.toString('hex');
-                        if (hash === user.password) {
-                        if (result) {
-                            // TODO: Generate a token or session for the user
-                            res.json({ message: 'Login successful', userId: user.id });
-                        } else {
-                            res.status(401).json({ error: 'Invalid credentials' });
-                        }
-                    });
-                } else {
-                    res.status(404).json({ error: 'User not found' });
-                }
-            }
-        });
-    },
-    loginUser: function(req, res) {
-        const { username, password } = req.body;
-        UserModel.getByUsername(username, function(err, user) {
-            if (err) {
-                res.status(500).json({ error: 'Error logging in', details: err });
             } else if (user) {
-                bcrypt.compare(password, user.password, function(err, result) {
-                    if (result) {
-                        // TODO: Generate a token or session for the user
-                        res.json({ message: 'Login successful', userId: user.UserID });
-                        const token = jwt.sign({ userId: user.UserID, username: username }, secretKey, { expiresIn: '1h' });
-                        res.json({ message: 'Login successful', userId: user.UserID, token: token });
+                crypto.pbkdf2(password, user.salt, 1000, 64, 'sha512', (err, derivedKey) => {
+                    if (err) {
+                        return res.status(500).json({ error: 'Error verifying password' });
+                    }
+                    const hash = derivedKey.toString('hex');
+                    if (hash === user.password) {
+                        const token = jwt.sign({ userId: user.id, username: username }, secretKey, { expiresIn: '1h' });
+                        res.json({ message: 'Login successful', userId: user.id, token: token });
                     } else {
                         res.status(401).json({ error: 'Invalid credentials' });
                     }
@@ -87,51 +60,22 @@ const UserController = {
     getUserById: function(req, res) {
         UserModel.getById(req.params.id, function(err, user) {
             if (err) {
-                res.status(500).json({ error: 'Error logging in', details: err });
+                res.status(500).json({ error: 'Error fetching user', details: err });
+            } else if (user) {
+                res.json(user);
             } else {
-                if (user) {
-                    crypto.pbkdf2(password, user.salt, 1000, 64, 'sha512', (err, derivedKey) => {
-                        if (err) {
-                            return res.status(500).json({ error: 'Error verifying password' });
-                        }
-                        const hash = derivedKey.toString('hex');
-                        if (hash === user.password) {
-                        if (result) {
-                            // TODO: Generate a token or session for the user
-                            res.json({ message: 'Login successful', userId: user.id });
-                        } else {
-                            res.status(401).json({ error: 'Invalid credentials' });
-                        }
-                    });
-                } else {
-                    res.status(404).json({ error: 'User not found' });
-                }
+                res.status(404).json({ error: 'User not found' });
             }
         });
     },
 
     addUser: function(req, res) {
+        // Assuming addUser is for admin purposes and doesn't require password hashing
         UserModel.add(req.body, function(err, user) {
             if (err) {
-                res.status(500).json({ error: 'Error logging in', details: err });
+                res.status(500).json({ error: 'Error adding user', details: err });
             } else {
-                if (user) {
-                    crypto.pbkdf2(password, user.salt, 1000, 64, 'sha512', (err, derivedKey) => {
-                        if (err) {
-                            return res.status(500).json({ error: 'Error verifying password' });
-                        }
-                        const hash = derivedKey.toString('hex');
-                        if (hash === user.password) {
-                        if (result) {
-                            // TODO: Generate a token or session for the user
-                            res.json({ message: 'Login successful', userId: user.id });
-                        } else {
-                            res.status(401).json({ error: 'Invalid credentials' });
-                        }
-                    });
-                } else {
-                    res.status(404).json({ error: 'User not found' });
-                }
+                res.status(201).json({ message: 'User added successfully', userId: user.id });
             }
         });
     },
@@ -139,27 +83,15 @@ const UserController = {
     deleteUser: function(req, res) {
         UserModel.delete(req.params.id, function(err, user) {
             if (err) {
-                res.status(500).json({ error: 'Error logging in', details: err });
-            } else {
-                if (user) {
-                    crypto.pbkdf2(password, user.salt, 1000, 64, 'sha512', (err, derivedKey) => {
-                        if (err) {
-                            return res.status(500).json({ error: 'Error verifying password' });
-                        }
-                        const hash = derivedKey.toString('hex');
-                        if (hash === user.password) {
-                        if (result) {
-                            // TODO: Generate a token or session for the user
-                            res.json({ message: 'Login successful', userId: user.id });
-                        } else {
-                            res.status(401).json({ error: 'Invalid credentials' });
-                        }
-                    });
-                } else {
-                    res.status(404).json({ error: 'User not found' });
-                }
+                res.status(500).json({ error: 'Error deleting user', details: err });
+            } else if (user) {
+                res.json({ message: 'User deleted successfully', userId: user.id });
             }
-        });
+            else {
+                res.status(404).json({ error: 'User not found' });
+            }
+        }
+        );
     },
 
     updateUser: function(req, res) {
